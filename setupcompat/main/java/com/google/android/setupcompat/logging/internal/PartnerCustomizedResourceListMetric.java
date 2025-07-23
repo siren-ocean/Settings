0ed2dc744877c6a82fc5ec221aca881086f847a8
Siren <yechengjian@xgd.com>
2025-07-23 13:50:11 +0800

    commit: add setupcompat

---
+++ setupcompat/main/java/com/google/android/setupcompat/logging/internal/PartnerCustomizedResourceListMetric.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.setupcompat.logging.internal;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build.VERSION_CODES;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import androidx.annotation.VisibleForTesting;
+import com.google.android.setupcompat.logging.CustomEvent;
+import com.google.android.setupcompat.logging.MetricKey;
+import com.google.android.setupcompat.logging.SetupMetricsLogger;
+
+/** Uses to log internal event customization resource list. */
+@TargetApi(VERSION_CODES.Q)
+public class PartnerCustomizedResourceListMetric {
+
+  public static void logMetrics(Context context, String deviceDisplayName, Bundle bundle) {
+    PersistableBundle logBundle =
+        buildLogBundleFromResourceConfigBundle(context.getPackageName(), deviceDisplayName, bundle);
+    if (!logBundle.isEmpty()) {
+      SetupMetricsLogger.logCustomEvent(
+          context,
+          CustomEvent.create(
+              MetricKey.get("PartnerCustomizationResource", "NoScreenName"), logBundle));
+    }
+  }
+
+  @VisibleForTesting
+  public static PersistableBundle buildLogBundleFromResourceConfigBundle(
+      String defaultPackageName, String deviceDisplayName, Bundle resourceConfigBundle) {
+    PersistableBundle persistableBundle = new PersistableBundle();
+    persistableBundle.putString("deviceDisplayName", deviceDisplayName);
+    for (String key : resourceConfigBundle.keySet()) {
+      Bundle resourceExtra = resourceConfigBundle.getBundle(key);
+      if (!resourceExtra.getString("packageName", defaultPackageName).equals(defaultPackageName)) {
+        persistableBundle.putBoolean(resourceExtra.getString("resourceName", key), true);
+      } else {
+        persistableBundle.putBoolean(resourceExtra.getString("resourceName", key), false);
+      }
+    }
+
+    return persistableBundle;
+  }
+}
